d2ad08710836dae6dff2a864950d3689d11eb62d
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-02-11 13:50:22 +0000

    Change the name of the project, set the entrypoint as being the main() function in script.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+"""
+Ryan Pepper (2018)
+
+This script sets configuration options for setuptools for the FolderAnalyse
+project.
+
+"""
 from setuptools import setup
 
 setup(
    name='folder-analyse',
    version='0.1',
-   description='A program that ',
+   description='A program that analyses files in a directory with '
+               'word frequency statistics.',
    author='Ryan Pepper',
    author_email='ryan.pepper@soton.ac.uk',
-   packages=['folder-analyse'],
+   packages=['FolderAnalyse'],
+   entry_points = {
+        'console_scripts': [ 'FolderAnalyse=FolderAnalyse.script:main' ]
+       }
 )
