a37a3f489f5faeb487a0bc1ad2d0bbace8728afe
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-02-11 12:18:40 +0000

    Add a parser to take options for the various functions

---
+++ FolderAnalyse/script.py
@@ -0,0 +1,38 @@
+"""
+Ryan Pepper (2018)
+
+This script contains the main entrypoint to the folder-analyse application.
+
+"""
+
+import argparse
+import os
+import sys
+#from process import process_file, process_dir
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="%(prog)s [options]"
+                    "Produce wordcount statistics for files in a directory."
+        )
+
+    parser.add_argument('path', type=str,
+                        help="Path to a directory or a file.")
+
+    parser.add_argument('-r', '--report', action='store_true',
+                        help="Generate a report about a directory")
+
+    parser.add_argument('-t', '--type', type=str, default='.txt',
+                        help="If path is to a directory, file extension\n"
+                             "of files that to be processed.")
+
+    parser.add_argument('-c', '--case-insensitive', action='store_true',
+                        help="By default, processing is case sensitive.\n"
+                             "Add this flag to make it case insensitive")
+
+    parser.add_argument('-e', '--exclude', type=str, nargs='+',
+                        help="Remove these strings from the text before\n"
+                             "processing.")
+
+    return parser.parse_args()
