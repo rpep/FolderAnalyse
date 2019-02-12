f1cbde57cdb409c631982873d9c75c5c73f6c44c
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-02-12 20:17:22 +0000

    Enable test running from __init__.py and parser

--- FolderAnalyse/__init__.py
+++ FolderAnalyse/__init__.py
@@ -1,2 +1,24 @@
+import pytest
+import glob
+import os
 name='FolderAnalyse'
+
+path = os.path.dirname(__file__)
+
+
+def runtests():
+    """
+    runtests()
+    
+    Run the test suite for FolderAnalyse
+    
+    Notes
+    -----
+    Adapted from:
+    https://docs.pytest.org/en/latest/usage.html#calling-pytest-from-python-code
+    """
+
+    from pytest import main
+    pytest.main(['-v', 'tests'])
+
 
